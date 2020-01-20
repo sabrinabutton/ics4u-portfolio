7439f318209fc57b9961b8d3a70bc46a0b77b65a
sabrinabutton <43886209+sabrinabutton@users.noreply.github.com>
2020-01-20 18:21:16 -0500

    Updates

---
+++ precache-manifest.67eb390df6606596e04cf03abfca3974.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8979652534f04bc9f25013a3c3eb874",
+    "url": "/ics4u-portfolio/index.html"
+  },
+  {
+    "revision": "ddf6277165c8bb741598",
+    "url": "/ics4u-portfolio/static/css/main.eb1c1239.chunk.css"
+  },
+  {
+    "revision": "141ae5ad8a1236adeac6",
+    "url": "/ics4u-portfolio/static/js/2.82795521.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/ics4u-portfolio/static/js/2.82795521.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddf6277165c8bb741598",
+    "url": "/ics4u-portfolio/static/js/main.f6902cd7.chunk.js"
+  },
+  {
+    "revision": "727a34f2198d9ab22f25",
+    "url": "/ics4u-portfolio/static/js/runtime-main.477e6c4c.js"
+  },
+  {
+    "revision": "7445f5143b0f7ae2dcf16e0589c25943",
+    "url": "/ics4u-portfolio/static/media/osc-rho.7445f514.PNG"
+  },
+  {
+    "revision": "7ebd8ad2fe0b0cfa627073937c2b378f",
+    "url": "/ics4u-portfolio/static/media/utopian2.7ebd8ad2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ics4u-portfolio/precache-manifest.561f2774305c16c974b74c6141d6dee5.js"
+  "/ics4u-portfolio/precache-manifest.67eb390df6606596e04cf03abfca3974.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ics4u-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
